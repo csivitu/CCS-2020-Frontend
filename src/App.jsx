4d39d2f67b25889d573ad35dc7b9ae8de9586a82
Subham Panda <im.sspanda10@gmail.com>
2020-12-14 14:45:47 +0530

    fix: made universal bg and added animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-// import { Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/footer/footer.component';
 import Header from './components/header/header.component';
 import ComingSoon from './pages/ComingSoon/ComingSoon';
-// import LandingPage from './pages/LandingPage/LandingPage';
+import LandingPage from './pages/LandingPage/LandingPage';
+
+import './App.css';
 
 function App() {
     return (
         <>
+            <div className="bg-div" />
             <Header />
             <ComingSoon />
             {/* <Route exact path="/">
