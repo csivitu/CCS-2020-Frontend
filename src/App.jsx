123c4f08e5254dc3fc7558d4cb8c416162a53530
ashikka <ashikagupta28@gmail.com>
2020-12-14 22:37:04 +0530

    feat: Resolved merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import HomePage from './pages/HomePage/HomePage';
 function App() {
     return (
         <>
+            <div className="bg-div" />
             <Header />
             {/* <ComingSoon /> */}
             <Route exact path="/">
