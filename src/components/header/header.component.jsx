babba8f6dd23ee996c055ed5057cc9e6ab8a72c9
Subham Panda <im.sspanda10@gmail.com>
2020-12-19 04:10:19 +0530

    feat: rules on navbar

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,12 +7,19 @@ const Header = () => {
     return (
         <div className="navbar-div d-flex align-items-center justify-content-between">
             <div className="right mx-5 my-auto d-flex justify-content-between align-items-center mx-5">
-                <a href={username.length > 0 ? '/domains' : '/'}>
+                <a
+                    href={username.length > 0 ? '/domains' : '/'}
+                    className="mr-4"
+                >
                     <h5 className="white link">HOME</h5>
                 </a>
-                <a href="/rules">
-                    <h5 className="white link">RULES</h5>
-                </a>
+                {username.length > 0 ? (
+                    <a href="/rules">
+                        <h5 className="white link">RULES</h5>
+                    </a>
+                ) : (
+                    ''
+                )}
             </div>
             <div className="left d-flex justify-content-between align-items-center mx-5">
                 {username.length > 0 ? (
