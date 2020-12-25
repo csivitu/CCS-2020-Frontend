2cf5d5cf453321a5ee144a2b2888b01aa2803e1e
Subham Panda <im.sspanda10@gmail.com>
2020-12-26 00:31:51 +0530

    feat: added 27 for slots

--- src/components/slotdatecard/index.jsx
+++ src/components/slotdatecard/index.jsx
@@ -6,7 +6,7 @@ import { nanoid } from '@reduxjs/toolkit';
 import { Card, Col, Row } from 'react-bootstrap';
 import './styles.css';
 
-const dates = [21, 22, 23, 24, 25, 26];
+const dates = [21, 22, 23, 24, 25, 26, 27];
 
 const SlotDateCard = (props) => {
     const { setDateWrap } = props;
